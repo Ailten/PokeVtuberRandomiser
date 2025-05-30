e33e4632d11313dbd3e4965bd802a4d897e2edd1
Ailten <faouzi26b@live.fr>
2025-05-30 18:14:40 +0200

    edit

--- JS/vtuber.js
+++ JS/vtuber.js
@@ -7,7 +7,10 @@ async function getRandomVtuber(){
     let vtuberObj = null;
 
     await fetch(urlJson)
-        .then(response => response.json())  
+        .then(response => {
+            console.log(response);
+            return response.json()
+        }) 
         .then(vtubers => {
             let randomIndex = Math.floor(Math.random() * vtubers.length);
             vtuberObj = vtubers[randomIndex];
