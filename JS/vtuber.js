d2e9a68fa0f3520d88d1277400efc68cb2190c0c
Ailten <faouzi26b@live.fr>
2025-05-30 18:13:19 +0200

    edit

--- JS/vtuber.js
+++ JS/vtuber.js
@@ -1,5 +1,5 @@
 
-const urlJson = "/JSON/vtuber.json"; //https://raw.githubusercontent.com/Ailten/PokeVtuberRandomiser/refs/heads/main/JSON/vtuber.json
+const urlJson = "https://raw.githubusercontent.com/Ailten/PokeVtuberRandomiser/refs/heads/main/JSON/vtuber.json";
 
 // ask a random vtuber.
 async function getRandomVtuber(){
@@ -7,10 +7,7 @@ async function getRandomVtuber(){
     let vtuberObj = null;
 
     await fetch(urlJson)
-        .then(response => {
-            console.log(response);
-            return response.json()
-        })  
+        .then(response => response.json())  
         .then(vtubers => {
             let randomIndex = Math.floor(Math.random() * vtubers.length);
             vtuberObj = vtubers[randomIndex];
